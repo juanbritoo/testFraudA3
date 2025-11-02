7007732c2964cddf100e3bbadba95d86d4df5388
Juan Brito <juanbritograngeiro@gmail.com>
2025-11-01 21:46:17 -0300

    removendo arquivo grande

--- algoritmo_python/tratamento_csv.py
+++ algoritmo_python/tratamento_csv.py
@@ -5,6 +5,7 @@ import os
 Primeiro passo é descompactar o arquivo e rodar este código, ele irá pegar a base de dados original
 fazer a exclusão das colunas desnecessárias e substituir o arquivo original. Isso para não ter um arquivo 
 pesado dentro da pasta.
+https://www.kaggle.com/datasets/kelvinkelue/credit-card-fraud-prediction
 '''
 
 caminho_original = r"C:\Users\juan_\Downloads\testFraudA3-main\algoritmo_python\A3 - Fraud Guard.csv"
