267c736be8745f9b37fa95b9fb784748c47ed588
Juan Brito <juanbritograngeiro@gmail.com>
2025-11-04 22:22:42 -0300

    Aplicando smote para balancear os dados de treino

---
+++ algoritmosPython/fase7_smoteBalance.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import os
+from imblearn.over_sampling import SMOTE
+
+
+X_train = pd.read_csv(r'C:\Users\juan_\Downloads\testFraudA3-main\algoritmosPython\X_train.csv')
+y_train = pd.read_csv(r'C:\Users\juan_\Downloads\testFraudA3-main\algoritmosPython\Y_train.csv')
+
+'''
+Objetivo: Aplicar nos dados de treino para equilibrar as classes is_fraud.
+'''
+
+print("Antes do SMOTE:")
+print(y_train['is_fraud'].value_counts())
+
+
+smote = SMOTE(random_state=42)
+X_train_res, y_train_res = smote.fit_resample(X_train, y_train)
+
+
+print("Depois do SMOTE:")
+print(y_train_res['is_fraud'].value_counts())
+
+pasta_destino = r'C:\Users\juan_\Downloads\testFraudA3-main\algoritmosPython'
+
+X_train_res.to_csv(os.path.join(pasta_destino,'X_train_balanced.csv'), index=False)
+y_train_res.to_csv(os.path.join(pasta_destino,'y_train_balanced.csv'), index=False)
+
+print("Balanceamento Concluído")
+
+#Execute no terminal: pip install imbalanced-learn
