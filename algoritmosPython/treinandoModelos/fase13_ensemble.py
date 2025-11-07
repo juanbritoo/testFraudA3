b419b8fc06e67060c0285dde606a631706227c6a
Juan Brito <juanbritograngeiro@gmail.com>
2025-11-06 22:55:53 -0300

    Fazendo Classificação com Esemble

---
+++ algoritmosPython/treinandoModelos/fase13_ensemble.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import os
+import joblib
+from sklearn.ensemble import VotingClassifier
+from sklearn.metrics import classification_report, confusion_matrix, roc_auc_score
+
+pasta_base = r"C:\Users\juan_\Downloads\testFraudA3-main\algoritmosPython"
+pasta_modelos = os.path.join(pasta_base, "treinandoModelos")
+
+X_test_path = os.path.join(pasta_base, "X_test.csv")
+y_test_path = os.path.join(pasta_base, "y_test.csv")
+
+X_test = pd.read_csv(X_test_path, sep=',', encoding='latin-1')
+y_test = pd.read_csv(y_test_path, sep=',', encoding='latin-1')
+
+if y_test.shape[1] > 1:
+    y_test = y_test.iloc[:, 0]
+
+print("Carregamento dos modelos treinados...:")
+
+modelo1 = joblib.load(os.path.join(pasta_modelos, "modelo1_logistic.pkl"))
+modelo2 = joblib.load(os.path.join(pasta_modelos, "modelo2_randomForest.pkl"))
+modelo3 = joblib.load(os.path.join(pasta_modelos, "modelo3_xgboost.pkl"))
+modelo4 = joblib.load(os.path.join(pasta_modelos, "modelo4_catboost.pkl"))
+modelo5 = joblib.load(os.path.join(pasta_modelos, "modelo5_mlp.pkl"))  
+
+print("Modelos carregados!")
+
+print("Criação do Ensamble")
+
+'''
+Objetivo: Criar um modelo de 'votação' onde cada modelo de IA, irá dar sua opnião probabilística, e cada modelo
+vota se é fraude ou não.
+'''
+
+ensemble_final = VotingClassifier(
+    estimators=[
+        ('logistic', modelo1),           
+        ('random_forest', modelo2),      
+        ('xgboost', modelo3),           
+        ('catboost', modelo4),                
+        ('mlp', modelo5),          
+                    
+    ],
+    voting='soft',                      
+    n_jobs=-1                           
+)
+
+print("Ensemble criado!")
+
+X_train_path = os.path.join(pasta_base, "X_train_balanced.csv")
+y_train_path = os.path.join(pasta_base, "y_train_balanced.csv")
+
+X_train = pd.read_csv(X_train_path, sep=',', encoding='latin-1')
+y_train = pd.read_csv(y_train_path, sep=',', encoding='latin-1')
+
+if y_train.shape[1] > 1:
+    y_train = y_train.iloc[:, 0]
+
+ensemble_final.fit(X_train, y_train)
+
+print("Ensemble treinado!")
+
+y_pred_ensemble = ensemble_final.predict(X_test)
+y_prob_ensemble = ensemble_final.predict_proba(X_test)[:, 1]
+
+print("\nRESULTADOS DO ENSEMBLE FINAL:")
+print("═" * 50)
+print("\nRelatório de Classificação:\n", classification_report(y_test, y_pred_ensemble))
+print("AUC do Ensemble:", roc_auc_score(y_test, y_prob_ensemble))
+print("\nMatriz de Confusão:\n", confusion_matrix(y_test, y_pred_ensemble))
+
+
+ensemble_path = os.path.join(pasta_modelos, "modelo_final_ensemble.pkl")
+joblib.dump(ensemble_final, ensemble_path)
+
+print("Sistema Fraud Guard Concluído!!")
